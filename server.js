01c9dbf5c9aac188fec4b16d6222267736b52f3e
Lynchk07 <lynch.kristinj@gmail.com>
2022-10-11 21:37:09 -0700

    fetchapiforbooks

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const path = require('path');
 const express = require('express');
 const handlebars = require('express-handlebars');
-const routes = require('./controllers');
+const routes = require('./index123');
 const helpers = require('./utils/helpers');
 const sequelize = require('./config/connection');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
