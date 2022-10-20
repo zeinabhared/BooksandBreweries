953e3dabccb3579f058eba3612a088076c464a05
Mona Lisa <zeinabhared@gmail.com>
2022-10-20 15:54:19 -0700

    small updatesto seeds

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -3,7 +3,7 @@ const { User } = require('../models');
 const userData = [
   {
     name: 'Dave',
-    email: 'Dave@gmail.com',
+    email: 'dave@gmail.com',
     password: "password12345"
   },
   {
@@ -14,17 +14,17 @@ const userData = [
   },
   {
     name: 'Jessica',
-    email: 'jessica123@hotmail.com',
+    email: 'jessica@hotmail.com',
     password: "password12345"
   },
   {
     name: 'Nichole',
-    email: 'nichole.22@hotmail.com',
+    email: 'nichole@hotmail.com',
     password: "password12345"
   },
   {
     name: 'Kimberly',
-    email: 'kim123@gmail.com',
+    email: 'kim@gmail.com',
     password: "password12345"
   },
 ];
