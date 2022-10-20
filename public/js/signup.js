f11a580a1f7fe07fde10cf9807cc112fac1d1207
Mona Lisa <zeinabhared@gmail.com>
2022-10-19 17:59:41 -0700

    fixed login and signup issues

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,10 +3,10 @@ const signupFormHandler = async (event) => {
 
     const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('.password-login').value.trim();
+    const password = document.querySelector('#password-field').value.trim();
 
     if (name && email && password) {
-        const response = await fetch('/api/user', {
+        const response = await fetch('/api/user/new', {
             method: 'POST',
             body: JSON.stringify({ name, email, password }),
             headers: { 'Content-Type': 'application/json' },
